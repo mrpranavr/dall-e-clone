f3aba6262d8c3c62ac20b37915115e4a275ab0fd
Pranav R <pranav81234@gmail.com>
2023-01-22 16:21:41 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+
+import connectDB from './mongodb/connect.js'
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
+
+// configing the system environment variables.
+dotenv.config()
+
+// Setting up the express application.
+const app = express()
+app.use(cors())
+app.use(express.json({limit: '50mb'}))
+
+app.use('/api/v1/post', postRoutes)
+app.use('/api/v1/dalle', dalleRoutes)
+
+app.get('/', async (req, res) => {
+    res.send('Hello from DALL-E!')
+})
+
+// Starting the server and connecting to the mongo DB database.
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGODB_URL)
+        app.listen(3000, () => console.log('Server has started on port http://localhost:3000'))
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+startServer()
